03cc4dbeaa5d61a0f6590910076c9ad1aedb7c1c
Bianca Pereira <BiancaPereira@users.noreply.github.com>
2025-04-12 11:53:29 -0300

    feat: adiciona sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,58 @@
+"use client";
+
+import { CircleDollarSign, CreditCard, HandCoins } from "lucide-react";
+import * as React from "react";
+
+import { NavMain } from "@/components/nav-main";
+import { Sidebar, SidebarContent, SidebarRail } from "@/components/ui/sidebar";
+
+const data = {
+  navMain: [
+    {
+      title: "Ganhos",
+      url: "/ganhos",
+      icon: HandCoins,
+    },
+    {
+      title: "Gastos",
+      url: "gastos",
+      icon: CircleDollarSign,
+      items: [
+        {
+          title: "Listar gastos fixos",
+          url: "/gastos-fixos",
+        },
+        {
+          title: "Adicionar gasto fixo",
+          url: "/gastos-fixos/adicionar",
+        },
+      ],
+    },
+    {
+      title: "Cartões de crédito",
+      url: "cartoes",
+      icon: CreditCard,
+      items: [
+        {
+          title: "Listar cartões de crédito",
+          url: "/cartoes-credito",
+        },
+        {
+          title: "Adicionar cartão de crédito",
+          url: "/cartoes-credito/adicionar",
+        },
+      ],
+    },
+  ],
+};
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+  return (
+    <Sidebar collapsible="icon" {...props}>
+      <SidebarContent>
+        <NavMain menuItems={data.navMain} />
+      </SidebarContent>
+      <SidebarRail />
+    </Sidebar>
+  );
+}
---
+++ components/nav-main.tsx
@@ -0,0 +1,90 @@
+"use client";
+
+import { ChevronRight, type LucideIcon } from "lucide-react";
+
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "@/components/ui/collapsible";
+import {
+  SidebarGroup,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarMenuSub,
+  SidebarMenuSubButton,
+  SidebarMenuSubItem,
+} from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
+
+export function NavMain({
+  menuItems,
+}: {
+  menuItems: {
+    title: string;
+    url: string;
+    icon?: LucideIcon;
+    items?: {
+      title: string;
+      url: string;
+    }[];
+  }[];
+}) {
+  const pathname = usePathname();
+
+  return (
+    <SidebarGroup>
+      <SidebarGroupLabel>Menu</SidebarGroupLabel>
+      <SidebarMenu>
+        {menuItems.map((item) => {
+          return item.items ? (
+            <Collapsible
+              key={item.title}
+              asChild
+              defaultOpen={pathname.includes(item.url)}
+              className="group/collapsible"
+            >
+              <SidebarMenuItem>
+                <CollapsibleTrigger asChild>
+                  <SidebarMenuButton tooltip={item.title}>
+                    {item.icon && <item.icon />}
+                    <span>{item.title}</span>
+                    <ChevronRight className="ml-auto transition-transform duration-200 group-data-[state=open]/collapsible:rotate-90" />
+                  </SidebarMenuButton>
+                </CollapsibleTrigger>
+                <CollapsibleContent>
+                  <SidebarMenuSub>
+                    {item.items?.map((subItem) => (
+                      <SidebarMenuSubItem key={subItem.title}>
+                        <SidebarMenuSubButton asChild>
+                          <a href={subItem.url}>
+                            <span
+                              className={
+                                pathname === subItem.url ? "font-bold" : ""
+                              }
+                            >
+                              {subItem.title}
+                            </span>
+                          </a>
+                        </SidebarMenuSubButton>
+                      </SidebarMenuSubItem>
+                    ))}
+                  </SidebarMenuSub>
+                </CollapsibleContent>
+              </SidebarMenuItem>
+            </Collapsible>
+          ) : (
+            <SidebarMenuButton key="item.url" tooltip={item.title}>
+              {item.icon && <item.icon />}
+              <a href={item.url}>
+                <span>{item.title}</span>
+              </a>
+            </SidebarMenuButton>
+          );
+        })}
+      </SidebarMenu>
+    </SidebarGroup>
+  );
+}
